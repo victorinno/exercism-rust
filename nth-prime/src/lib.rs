521d4b3fbe0c5a6ca627c13401fa4bac68d3c38f
che <che@eclipse.org>
2020-04-22 13:10:55 +0000

    adding the 4th teste

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -4,7 +4,7 @@ pub fn is_prime(n: u32) -> bool {
 
 pub fn nth(n: u32) -> u32 {
     let r = match n {
-        n if n <= 0 => Some(2),
+        n if n == 0 => Some(2),
         n => (1..).filter(|c| is_prime(*c)).nth((n as usize) + 1),
     };
     return r.unwrap();
