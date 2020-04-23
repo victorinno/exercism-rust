ed50915a8b749579829f797b9aead46a29994679
che <che@eclipse.org>
2020-04-23 16:42:51 +0000

    finished the proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.len() == 0 {
+        return String::new();
+    }
+    let frases = (0..list.len()-1)
+        .map(|n| (list[n], list[n+1]))
+        .map(|x| format!("For want of a {} the {} was lost.", x.0, x.1))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    match frases {
+        frases if frases == "" => format!("And all for the want of a {}.",list[0]),
+        frases => format!("{}\nAnd all for the want of a {}.", frases, list[0]) 
+    }
+    
 }
