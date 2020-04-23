d0da16be287eb6d4535057fd1f4a56f1f8bc1846
che <che@eclipse.org>
2020-04-23 16:56:59 +0000

    end of difference of squares

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,14 +1,13 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    let result: u32 = (1..n+1).fold(0, |acc, x| acc + x).pow(2);
+    return result;
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    let result: u32 = (1..n+1).map(|x| x.pow(2)).fold(0, |acc, x| acc + x);
+    return result;
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n) - sum_of_squares(n)
 }
