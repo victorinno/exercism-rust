b69b65be6b0222e8c89e8aba47a7af8cf7150617
che <che@eclipse.org>
2020-04-24 12:49:33 +0000

    finished grain exercise

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    let two: u64 = 2;
+    match s {
+        s if s > 64 || s < 1 => panic!("Square must be between 1 and 64"),
+        s => two.pow(s - 1),
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..65).map(|x| square(x)).fold(0, |acc, x| acc + x)
 }
