a2573f291e857900d66331c4d53573cd21d9dca3
victorinno <rotc21@gmail.com>
2020-04-27 21:30:57 +0100

    finished primes of a number

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    if n == 1 {
+        return Vec::new();
+    }
+    let mut dividend: u64 = n;
+    let mut factors: Vec<u64> = Vec::new();
+    let mut prime_divisor: u64 = 2;
+    while dividend > 1 {
+        if dividend % prime_divisor == 0 {
+            factors.push(prime_divisor);
+            dividend /= prime_divisor;
+        } else {
+            prime_divisor += 1;
+        }
+    }
+    factors
 }
---
+++ prime-factors/src/main.rs
@@ -0,0 +1,8 @@
+mod lib;
+
+use lib::factors;
+
+pub fn main() {
+    factors(1);
+    factors(2);
+}
