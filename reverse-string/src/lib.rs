ad0cb03128b93359f610e9f56a20f77d920dfcc8
victorinno <rotc21@gmail.com>
2020-05-02 19:39:06 +0100

    graphemes

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,17 +1,9 @@
-
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
     if "" == input {
         String::new();
     }
-    input.chars().rev().collect()
+    input.graphemes(true).rev().collect()
 }
-// I used this one to try a better performance
-// extern crate unicode_segmentation;
-// use unicode_segmentation::UnicodeSegmentation;
-// pub fn reverse2(str_in: &str) -> String {
-//     str_in
-//         .graphemes(true)
-//         .rev()
-//         .flat_map(|c| c.chars())
-//         .collect()
-// }
+
