6ea68817be7294a80d5e42420cb4d184f848a332
victorinno <rotc21@gmail.com>
2020-05-05 10:50:27 +0100

    init high scores

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -3,6 +3,10 @@ fn power_9(base:i64) -> i64 {
     base.pow(9)
 }
 
+fn power( power: u32) ->  dyn Fn<i64, Output = i64>  {
+    |base: i64| base.pow(power)
+} 
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
     start + Duration::seconds(power_9(10))
