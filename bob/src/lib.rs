332241cc30e89d0ab876bf1dbfcea19136665494
victorinno <rotc21@gmail.com>
2020-05-08 16:26:58 +0100

    v2

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -5,14 +5,14 @@ pub fn reply(message: &str) -> &str {
     let treated_message = treated_message.trim();
     match treated_message {
         m if m.len() == 0 => "Fine. Be that way!",
-        m if m.to_uppercase() == m.clone()
+        m if m.to_uppercase() == m
             && m.get(m.len() - 1..).unwrap() == "?"
             && Regex::new(r"[a-zA-Z]").unwrap().is_match(&m) =>
         {
             "Calm down, I know what I'm doing!"
         }
         m if m.get(m.len() - 1..).unwrap() == "?" => "Sure.",
-        m if m.to_uppercase() == m.clone() && Regex::new(r"[a-zA-Z]").unwrap().is_match(&m) => {
+        m if m.to_uppercase() == m && Regex::new(r"[a-zA-Z]").unwrap().is_match(&m) => {
             "Whoa, chill out!"
         }
         _ => "Whatever.",
