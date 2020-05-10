229e3c6486505cd122afaa8d1476fb09256bac41
victorinno <rotc21@gmail.com>
2020-05-10 22:01:20 +0100

    improvments

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,56 +1,13 @@
-#[derive(Debug)]
-struct Stack {
-    top: i32,
-    items: Vec<char>,
-}
-
-impl Stack {
-    pub fn push(&mut self, x: &char) {
-        self.items.push(x.clone());
-        self.top += 1;
-    }
-
-    pub fn pop(&mut self) -> char {
-        let element: char = match self.items.pop() {
-            None => '\0',
-            Some(e) => e,
-        };
-        self.top -= 1;
-        return element;
-    }
-
-    pub fn is_empty(&mut self) -> bool {
-        return self.top == -1;
-    }
-}
-
-fn is_matching_pair(character1: char, character2: char) -> bool {
-    if character1 == '(' && character2 == ')' {
-        return true;
-    } else if character1 == '{' && character2 == '}' {
-        return true;
-    } else if character1 == '[' && character2 == ']' {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut stack: Stack = Stack {
-        top: -1,
-        items: Vec::new(),
-    };
+    let mut brackets: Vec<char> = Vec::new();
     for c in string.chars() {
-        if c == '{' || c == '(' || c == '[' {
-            stack.push(&c);
-        } else if c == '}' || c == ')' || c == ']' {
-            if stack.is_empty() {
-                return false;
-            } else if !is_matching_pair(stack.pop(), c) {
-                return false;
-            }
+        match c {
+            '(' | '{' | '[' => brackets.push(c),
+            ')' => if brackets.pop() != Some('(') {return false},
+            ']' => if brackets.pop() != Some('[') {return false},
+            '}' => if brackets.pop() != Some('{') {return false},
+            _ => ()
         }
     }
-    return stack.is_empty();
+    brackets.is_empty()
 }
