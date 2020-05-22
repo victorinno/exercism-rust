06df4e5c18f95655dc70e27986a9fde874d09c87
victorinno <rotc21@gmail.com>
2020-05-22 14:04:37 +0100

    optimized solution

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,68 +1,27 @@
-use core::fmt::Display;
+use std::fmt;
 
-#[derive(PartialEq, Debug)]
+const DAY: i64 = 24 * 60;
+const HOUR: i64 = 60;
+
+#[derive(Debug, Eq, PartialEq)]
 pub struct Clock {
-    hours: i32,
-    minutes: i32,
+    minutes: i64, 
 }
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
+    pub fn new(hours: i64, minutes: i64) -> Clock {
         Clock {
-            hours: 0,
-            minutes: 0,
-        }
-        .add_hour(hours)
-        .add_minutes(minutes)
-    }
-
-    pub fn add_hour(&self, hours: i32) -> Self {
-        let mut h = self.hours + hours;
-        while h > 23 {
-            h -= 24;
-        }
-        while h < 0 {
-            h += 24;
-        }
-        Clock {
-            hours: h,
-            minutes: self.minutes,
+            minutes: (((hours * HOUR + minutes) % DAY) + DAY) % DAY
         }
     }
 
-    pub fn add_minutes(&mut self, minutes: i32) -> Self {
-        self.minutes = self.minutes + minutes;
-        if self.minutes >= 60 {
-            while self.minutes >= 60 {
-                self.minutes -= 60;
-                self.hours += 1;
-            }
-
-            while self.hours > 23 {
-                self.hours -= 24;
-            }
-        } else {
-            while self.minutes < 0 {
-                self.minutes += 60;
-                self.hours -= 1;
-            }
-
-            while self.hours < 0 {
-                self.hours = self.hours + 24;
-            }
-        }
-        Clock {
-            hours: self.hours,
-            minutes: self.minutes,
-        }
+    pub fn add_minutes(self, minutes: i64) -> Clock {
+        Clock::new(0, self.minutes + minutes)
     }
 }
 
-impl Display for Clock {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        let minutes: i32 = self.minutes;
-        let hour: i32 = self.hours;
-
-        write!(f, "{:0>2}:{:0>2}", hour, minutes)
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.minutes / HOUR, self.minutes % HOUR)
     }
 }
